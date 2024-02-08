3d7a1c256d76dc6d08db52afbe7859b796532fdb
anhdung2k1 <19521167@gm.uit.edu.vn>
2024-02-08 17:57:07 +0700

    change folder name

--- server/controller/ServerController.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class ServerController {
-    public:
-        static void connectToClient();
-}
--- server/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "controller/ServerController.h"
-#include "utils/CurlUtils.h"
-using namespace std;
-
-int main() {
-    ServerController::connectToClient();
-    return 0;
-}
