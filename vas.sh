def8c3f91c7e2661d48a29253fe4a3b1725ff388
anhdung2k1 <19521167@gm.uit.edu.vn>
2024-06-30 00:14:25 +0700

    add config metallb

--- vas.sh
+++ vas.sh
@@ -101,6 +101,10 @@ build_all() {
 }
 
 get_version() {
+    if [[ -n $BUILD_DIR/var/.version ]]; then
+    	cat $BUILD_DIR/var/.version
+	exit 0
+    fi
     suffix=$(git rev-parse HEAD | sed 's/^0*//g' | cut -c1-7 | tr 'a-f' '1-6')
     suffix+=$(git diff --quiet && git diff --cached --quiet || echo '9999')
     echo "$(<$VAS_GIT/VERSION_PREFIX)-${suffix}"
