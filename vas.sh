04eafa09ef0ad80c4148a9a3d178cd2b80bcd1a0
anhdung2k1 <19521167@gm.uit.edu.vn>
2024-07-20 04:28:26 +0700

    update cert

--- vas.sh
+++ vas.sh
@@ -196,8 +196,6 @@ build_repo() {
             echo "Copy requirements.txt to $__name"
             cp -f $VAS_GIT/requirements.txt $DOCKER_DIR/$__name \
             || die "Unable to copy the requirements.txt"
-            cp -rf $BUILD_DIR $DOCKER_DIR/$__name \
-            || die "Unable to copy the $BUILD_DIR"
         fi
 
         echo "Copy folder $__name to docker"
